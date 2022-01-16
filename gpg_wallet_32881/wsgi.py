2cb6956b0932a0f0be93b20d134259b7524c0f70
Crowdbotics <team@crowdbotics.com>
2022-01-16 19:47:32 +0000

    Initial commit.

---
+++ gpg_wallet_32881/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gpg_wallet_32881 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gpg_wallet_32881.settings')
+
+application = get_wsgi_application()
